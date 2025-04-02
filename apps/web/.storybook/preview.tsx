0d9286ca19c1df09ffa080ba56c74c00d8641a18
Brett Cimbalik <bcimbali@gmail.com>
2025-04-02 14:41:25 -0400

    feat(stories): use storybook/addon-themes

--- apps/web/.storybook/preview.tsx
+++ apps/web/.storybook/preview.tsx
@@ -1,10 +1,8 @@
 import type { Preview } from '@storybook/react';
 import '../../web/src/app/global.css';
+import { withThemeByClassName } from '@storybook/addon-themes';
 
 export const parameters = {
-  backgrounds: {
-    default: 'light'
-  },
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
@@ -20,31 +18,18 @@ export const parameters = {
   }
 };
 
-const isColorLight = (hex: string) => {
-  const hexColor = hex.replace(/^#/, '');
-  // Parse r, g, b values
-  const r = parseInt(hexColor.substring(0, 2), 16);
-  const g = parseInt(hexColor.substring(2, 4), 16);
-  const b = parseInt(hexColor.substring(4, 6), 16);
-
-  // Calculate luminance
-  const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-
-  // Return true if color is light, false if dark
-  return luminance > 128;
-};
-
 const preview: Preview = {
   parameters,
   decorators: [
+    withThemeByClassName({
+      themes: {
+        light: 'light',
+        dark: 'dark'
+      },
+      defaultTheme: 'light'
+    }),
     (Story, ctx) => {
-      const bgColor = ctx?.globals?.backgrounds?.value ?? '#fff';
-      const containerClass = isColorLight(bgColor) ? 'light' : 'dark';
-      return (
-        <div className={containerClass}>
-          <Story />
-        </div>
-      );
+      return <Story />;
     }
   ]
 };
