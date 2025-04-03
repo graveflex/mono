92ee373d7cb76374805ec60f6e896e875b3d9e1e
Brett Cimbalik <bcimbali@gmail.com>
2025-04-03 14:47:07 -0400

    feat(stories): add mockData for HeeadrSectionStory

--- apps/web/src/blocks/HeaderSectionsBlock/HeaderSectionsBlock.stories.tsx
+++ apps/web/src/blocks/HeaderSectionsBlock/HeaderSectionsBlock.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import type { HeaderSectionsBlockType } from '.';
 import HeaderSectionsBlock from '.';
+import headerSectionsMockData from './HeaderSectionsMockData';
 
 import MakeVariations from '@mono/web/payload/utils/makeStoryBookVariations';
 
@@ -21,11 +22,13 @@ type Story = StoryObj<HeaderSectionsBlockType>;
 
 export const Variant1: Story = {
   args: {
-    variant: '1'
+    variant: '1',
+    content: headerSectionsMockData.data
   }
 };
 export const Variant2: Story = {
   args: {
-    variant: '2'
+    variant: '2',
+    content: headerSectionsMockData.data
   }
 };
