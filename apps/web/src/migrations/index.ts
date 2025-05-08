863dfcb053ebb53f79f18f72caddd5bd767dd0ed
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2025-05-08 10:57:58 -0500

    db: reset migrations

--- apps/web/src/migrations/index.ts
+++ apps/web/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250508_160309 from './20250508_160309';
+import * as migration_20250508_160354 from './20250508_160354';
 
 export const migrations = [
   {
-    up: migration_20250508_160309.up,
-    down: migration_20250508_160309.down,
-    name: '20250508_160309'
+    up: migration_20250508_160354.up,
+    down: migration_20250508_160354.down,
+    name: '20250508_160354'
   },
 ];
